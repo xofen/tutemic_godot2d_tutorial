63ace9c1461096d80fdb8dbe3863c441ded0aece
xofen <87589625+xofen@users.noreply.github.com>
2023-12-20 09:15:12 -0300

    no_money_for_war_alpha

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -4,15 +4,22 @@ extends Node2D
 
 @export var projetil_inimigo_tcns: PackedScene
 
+func atirar_agora():
+	var tiro = projetil_inimigo_tcns.instantiate()
+	add_sibling(tiro)
+	tiro.position = self.position
+	print("BOOOMBA!")
+
 func _process(delta):
 	position.x += speed * delta
 	
-	if position.x > 5:
-		var tiro = projetil_inimigo_tcns.instantiate()
-		add_sibling(tiro)
-		tiro.position = self.position
+	var rand_tiro = randi_range(20, 1000)
+	if round(position.x) == rand_tiro:
+		atirar_agora()
 
 
 func _on_area_entered(other_area):
 	if other_area.is_in_group("player"):
 		self.queue_free()
+		
+		GameState.score += 152.817341278364612973691273465912734651
--- scripts/projetil_inimigo.gd
+++ scripts/projetil_inimigo.gd
@@ -9,3 +9,4 @@ func _process(delta):
 func _on_area_entered(other_area):
 	if other_area.is_in_group("player"):
 		self.queue_free()
+		GameState.score += 74.5346135645675672456724572456724672467
